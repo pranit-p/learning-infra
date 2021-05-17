107fbddd8fb566aab59d3f88238d2f407b00fbb0
pranit pawar <82363887+pranit-p@users.noreply.github.com>
2021-05-17 23:16:49 +0530

    add IAM role instead of s3 bucket

--- main.tf
+++ main.tf
@@ -2,28 +2,26 @@ provider "aws" {
   region = "us-east-1"
 }
 
+resource "aws_iam_role" "test_role" {
+  name = "test_role"
 
-resource "aws_s3_bucket_public_access_block" "example" {
-  bucket = aws_s3_bucket.log_bucket.id
+  # Terraform's "jsonencode" function converts a
+  # Terraform expression result to valid JSON syntax.
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      },
+    ]
+  })
 
-  block_public_acls   = true
-  block_public_policy = true
-}
-
-
-resource "aws_s3_bucket" "log_bucket" {
-  bucket = "my-tf-log-bucket-demo1"
-  acl    = "log-delivery-write"
-
-}
-
-resource "aws_s3_bucket" "b" {
-  bucket = "my-tf-test-bucket2123"
-  acl    = "private"
-
-  logging {
-    target_bucket = aws_s3_bucket.log_bucket.id
-    target_prefix = "log/"
+  tags = {
+    tag-key = "tag-value"
   }
 }
-
