e84a6f264d64ccaac1b1f4bf0114d7b6026dea03
pranit pawar <82363887+pranit-p@users.noreply.github.com>
2021-05-19 23:39:27 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,8 @@
 terraform {
   backend "s3" {
     bucket         = "persnal-terraform-backend"
-    path = "./terraform.tfstate"
+    key = "./terraform.tfstate"
+    region = "us-east-1"
   }
 }
+
