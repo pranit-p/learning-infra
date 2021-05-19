5062857c79ed1074dc8ba8d28615dff4b4f04750
pranit <pranit.pawar@medlypharmacy.com>
2021-05-19 10:46:33 +0530

    clonning changes from github site

--- main.tf
+++ main.tf
@@ -2,25 +2,25 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_s3_bucket" "codebuild-bucket" {
-  bucket = "codebuild_bucket"
-  acl    = "private"
-  versioning {
-    enabled = true
-  }
-  tags = {
-    Name        = "My bucket"
-    Environment = "Dev"
-  }
-  server_side_encryption_configuration {
-    rule {
-      apply_server_side_encryption_by_default {
-        sse_algorithm     = "AES256"
-      }
-    }
-  }
-  logging {
-    target_bucket = "logging_bucket"
-  }
-}
-
+//resource "aws_s3_bucket" "codebuild-bucket" {
+//  bucket = "codebuild_bucket"
+//  acl    = "private"
+//  versioning {
+//    enabled = true
+//  }
+//  tags = {
+//    Name        = "My bucket"
+//    Environment = "Dev"
+//  }
+//  server_side_encryption_configuration {
+//    rule {
+//      apply_server_side_encryption_by_default {
+//        sse_algorithm     = "AES256"
+//      }
+//    }
+//  }
+//  logging {
+//    target_bucket = "logging_bucket"
+//  }
+//}
+//
