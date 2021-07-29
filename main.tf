19512f73a06dc2bd102496ab685ff4f742f3a340
pranit <pranit.pawar@medly.com>
2021-07-29 14:39:44 +0530

    add one bug

--- main.tf
+++ main.tf
@@ -2,47 +2,6 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_iam_role" "test1212_role_testple_test" {
-  name = "test_role_testple_test"
-
-  # Terraform's "jsonencode" function converts a
-  # Terraform expression result to valid JSON syntax.
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = "sts:AssumeRole"
-        Effect = "Allow"
-        Sid    = ""
-        Principal = {
-          Service = "ec2.amazonaws.com"
-        }
-      },
-    ]
-  })
-
-  tags = {
-    tag-key = "tag-value"
-  }
-}
-
-
-
-resource "aws_iam_role" "demo-for-codebuild-test-example_test" {
-  name = "example_codebuild_test_test"
-
-  assume_role_policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Effect": "Allow",
-      "Principal": {
-        "Service": "codebuild.amazonaws.com"
-      },
-      "Action": "sts:AssumeRole"
-    }
-  ]
-}
-EOF
+resource "aws_s3_bucket" "b" {
+  bucket = "my-tf-test-bucket"
 }
