d29ea849067c58b1387b6b97704f01547262b8bc
pranit <pranit.pawar@medly.com>
2021-07-29 17:57:51 +0530

    added on bug

--- main.tf
+++ main.tf
@@ -11,3 +11,13 @@ resource "aws_s3_bucket" "b" {
     Environment = "Dev"
   }
 }
+
+resource "aws_s3_bucket" "dbkdsb" {
+  bucket = "my-tf-test-bucketkbsakdka"
+  acl    = "private"
+
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
+}
