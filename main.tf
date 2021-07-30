62a73e732d4902cedab04da71e1c678d5516dd1c
pranit <pranit.pawar@medly.com>
2021-07-30 13:24:24 +0530

    added one bug

--- main.tf
+++ main.tf
@@ -1,3 +1,21 @@
 provider "aws" {
   region = "us-east-1"
 }
+
+resource "aws_security_group" "allow_tls" {
+  name = "allow_tls"
+
+
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "allow_tls"
+  }
+}
