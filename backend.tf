1a7c5407224b38a61f6d1b4653c3dc8acd041dc4
pranit <pranit.pawar@medly.com>
2021-08-01 23:47:16 +0530

    added one bug also

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket  = "persnal-backed-bucket-for-all-repo"
-    key = "terraform/demo"
+    bucket = "persnal-backed-bucket-for-all-repo"
+    key    = "terraform/demo"
     region = "us-east-1"
   }
 }
--- module/codebuild/main.tf
+++ module/codebuild/main.tf
@@ -1,16 +1,16 @@
 resource "aws_codebuild_project" "demo-code-built" {
-  name = "demo-project"
+  name         = "demo-project"
   service_role = aws_iam_role.demo_iam_role.arn
   artifacts {
     type = "NO_ARTIFACTS"
   }
   environment {
     compute_type = "BUILD_GENERAL1_SMALL"
-    image = "aws/codebuild/standard:1.0"
-    type = "LINUX_CONTAINER"
+    image        = "aws/codebuild/standard:1.0"
+    type         = "LINUX_CONTAINER"
   }
   source {
-    type = "GITHUB"
+    type     = "GITHUB"
     location = "https://github.com/pranit-p/demo-react-app.git"
   }
 }
