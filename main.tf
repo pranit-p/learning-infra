b3f812a0034876c0fa4df2f39790c7a7b24d0e17
pranit <pranit.pawar@medly.com>
2021-08-01 23:43:34 +0530

    one bug added

--- main.tf
+++ main.tf
@@ -1,3 +1,30 @@
 provider "aws" {
   region = "us-east-1"
 }
+
+
+resource "aws_security_group" "allow_tls" {
+  name        = "allow_tls"
+  description = "Allow TLS inbound traffic"
+
+  ingress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "allow_tls"
+  }
+}
