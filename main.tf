feb193b583a9de6557807f368b9d0b65e497fcf2
pranit <pranit.pawar@medly.com>
2021-08-04 15:04:12 +0530

    added one more bug for check s3

--- main.tf
+++ main.tf
@@ -2,20 +2,53 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_s3_bucket" "example" {
-	bucket = "example"
-	acl = "private-read"
+resource "aws_s3_bucket" "dph_uploads" {
+  bucket = "demo-1-dph-uploads"
+  acl    = "private"
+  versioning {
+    enabled = true
+  }
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
 }
+
+resource "aws_s3_bucket" "dp_uploads" {
+  bucket = "demo-1-dp-uploads"
+  acl    = "private"
+  versioning {
+    enabled = true
+  }
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
   
-resource "aws_s3_bucket_public_access_block" "example" {
-	bucket = aws_s3_bucket.example.id
-	block_public_acls   = true
-	block_public_policy = true
+}
+
+locals {
+  buckets_to_block_public_access = toset([
+     aws_s3_bucket.ph_uploads.id,
+     aws_s3_bucket.dph_uploads.id
+  ])
 }
 
 
-
-
+resource "aws_s3_bucket_public_access_block" "s3_bucket_block_public_access" {
+  for_each                = local.buckets_to_block_public_access
+  bucket                  = each.key
+  block_public_acls       = true
+  block_public_policy     = true
+  restrict_public_buckets = true
+  ignore_public_acls      = true
+}
 
 
 
