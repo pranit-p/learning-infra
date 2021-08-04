82fe1007f5fe1ad8b662b034ce9bd3b5abacf27d
pranit <pranit.pawar@medly.com>
2021-08-04 14:59:29 +0530

    solve tfsec problem

--- main.tf
+++ main.tf
@@ -2,30 +2,54 @@ provider "aws" {
   region = "us-east-1"
 }
 
-
-#tfsec:ignore:AWS008
-resource "aws_security_group" "allow_tls" {
-  name        = "allow_tls"
-  description = "Allow TLS inbound traffic"
-
-  ingress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
-
-  }
-
-  egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
-  }
-
-  tags = {
-    Name = "allow_tls"
-  }
+resource "aws_s3_bucket" "example" {
+	bucket = "example"
+	acl = "private-read"
 }
+  
+resource "aws_s3_bucket_public_access_block" "example" {
+	bucket = aws_s3_bucket.example.id
+	block_public_acls   = true
+	block_public_policy = true
+}
+
+
+
+
+
+
+
+
+
+
+//resource "aws_secretsmanager_secret" "bad_example" {
+//  name       = "lambda_password"
+//}
+
+//resource "aws_alb" "good_example" {  }
+#tfsec:ignore:AWS008
+//resource "aws_security_group" "allow_tls" {
+//  name        = "allow_tls"
+//  description = "Allow TLS inbound traffic"
+//
+//  ingress {
+//    from_port        = 0
+//    to_port          = 0
+//    protocol         = "-1"
+//    cidr_blocks      = ["0.0.0.0/0"]
+//    ipv6_cidr_blocks = ["::/0"]
+//
+//  }
+//
+//  egress {
+//    from_port        = 0
+//    to_port          = 0
+//    protocol         = "-1"
+//    cidr_blocks      = ["0.0.0.0/0"]
+//    ipv6_cidr_blocks = ["::/0"]
+//  }
+//
+//  tags = {
+//    Name = "allow_tls"
+//  }
+//}
