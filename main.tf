2f89cc8903cc3e6b259b6e714a559e9369421326
pranit-p <pranit.pawar@medly.com>
2022-01-31 22:07:32 +0530

    tfsec fix all errors

--- main.tf
+++ main.tf
@@ -5,21 +5,31 @@ provider "aws" {
 resource "aws_s3_bucket" "bwegfe" {
   bucket = "my-tf-test-bucketergerj"
   acl    = "private"
-
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        kms_master_key_id = "arn"
+        sse_algorithm     = "aws:kms"
+      }
+    }
+  }
+  versioning {
+    enabled = true
+  }
+  logging {
+    target_bucket = "target-bucket"
+  }
   tags = {
     Name        = "My bucket"
     Environment = "Dev"
   }
 }
 
-
-
-resource "aws_s3_bucket" "b" {
-  bucket = "my-tf-test-bucket"
-  acl    = "private"
-
-  tags = {
-    Name        = "My bucket"
-    Environment = "Dev"
-  }
+resource "aws_s3_bucket_public_access_block" "good_example" {
+  bucket                  = aws_s3_bucket.bwegfe.id
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
 }
+
