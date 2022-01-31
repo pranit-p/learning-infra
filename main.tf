3aaa230b059d4a0913843a7874e1d5de23178f99
pranit-p <pranit.pawar@medly.com>
2022-01-31 21:58:29 +0530

    tfsec look

--- main.tf
+++ main.tf
@@ -2,6 +2,15 @@ provider "aws" {
   region = "us-east-1"
 }
 
+resource "aws_s3_bucket" "bwegfe" {
+  bucket = "my-tf-test-bucketergerj"
+  acl    = "private"
+
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
+}
 
 
 
